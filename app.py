21180700f18f10425b5c3127dfaf9759ffb358b9
Nalin Kularathna <pgnaleen@gmail.com>
2020-12-26 08:49:20 +0000

    change to 2

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  return "ase cloud assignment mock service 1\n"
+  return "ase cloud assignment mock service 2\n"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
--- test_app.py
+++ test_app.py
@@ -18,7 +18,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "ase cloud assignment mock service 1\n")
+    self.assertEqual(hello(), "ase cloud assignment mock service 2\n")
 
 if __name__ == '__main__':
   unittest.main()
